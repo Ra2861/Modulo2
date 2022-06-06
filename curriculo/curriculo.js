07ccfa9a26af1d7cdb69272068292df71f594670
Ra2861 <99209068+Ra2861@users.noreply.github.com>
2022-06-05 21:55:02 -0300

    curriculo Jquery

---
+++ curriculo/curriculo.js
@@ -0,0 +1,26 @@
+alert("Bem-vindo ao meu curriculo")
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+  // painel descer e subir
+  $(document).ready(function(){
+	$("#flip").click(function(){
+	  $("#panel").slideDown("slow");
+	});
+  });
+
