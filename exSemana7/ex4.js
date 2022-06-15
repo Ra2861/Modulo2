e370551d26bb674444fdfebd701673f8254fea48
Ra2861 <99209068+Ra2861@users.noreply.github.com>
2022-06-15 14:26:53 -0300

    Add files via upload

--- exSemana7/ex4.js
+++ exSemana7/ex4.js
@@ -1,26 +1,37 @@
-function desconto(){
+
+function desconto() {
     var pnumero = parseInt(document.getElementById('pessoas').value);
     var etinerario = document.getElementById('hora');
+    var diurno = document.getElementById('diurno');
+    var noturno = document.getElementById('noturno');
 
-    if (etinerario == diurno ) {
-        document.getElementById('resultado') = 200 +"Reais por pessoa"
-        
-    }
 
-    if (etinerario == diurno && pnumero >= 50 ){
-        document.getElementById('resultado') = 160 +"Reais por pessoa"
+    if (etinerario = diurno && pnumero > 50)
+        document.getElementById('resultado').innerHTML = pnumero * 160 + " Reais por pessoa "
 
-    }
 
-    if ( etinerario== noturno) {
-        document.getElementById('resultado') = 500 +"Reais por pessoa, desconto de 40%"
-    }
+    else (etinerario = diurno)
+    document.getElementById('resultado').innerHTML = pnumero * 200 + " Reais por pessoa"
 
-    if (etinerario == noturno && pnumero >= 50 ){
-        document.getElementById('resultado') = 250 +"Reais por pessoa, desconto de 50%"
 
-    }
+    if (etinerario = noturno && pnumero > 50)
+        document.getElementById('resultado').innerHTML = pnumero * 250 + " Reais por pessoa"
+
+
+    else (etinerario = noturno)
+    document.getElementById('resultado').innerHTML = pnumero * 500 + " Reais por pessoa"
+
+
 
 
 
 }
+
+
+
+
+/*function somaV() {
+    var quantia = parseInt(document.getElementById('quantia').value);
+    document.getElementById('quantia').value = quantia + 1;
+}*/
+
--- exSemana7/ex5.js
+++ exSemana7/ex5.js
@@ -1,3 +1,13 @@
-function nota(){ 
-var quantidade de alunos= parseInt(document.getElementById('quant de alunos'))
+function nota() {
+    var quant = parseInt(document.getElementById('quant de alunos').value);
+    var numero = parseInt(document.getElementById('numero').value);
+    var prova = parseInt(document.getElementById('prova').value);
+    var trabalho = parseInt(document.getElementById('trabalho').value)
+
+    document.getElementById('mTurma') = "a média da turma é" + prova+trabalho/quant 
+
+    
+
+
+
 }
